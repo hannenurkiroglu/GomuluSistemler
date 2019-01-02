88a65dd8a4ff25ee1a5b67360e2c5fa39ebfc867
Hannenur <hannenur.yazbahar@bil.omu.edu.tr>
2019-01-02 12:46:50 +0300

    Add files via upload

---
+++ gcd/gcd.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int gcd(int a, int b);
+
+int main(void){
+  int a=12, b=56;
+  printf("%d\n", gcd(a,b));
+  return 0;
+}
---
+++ gcd/gcd2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int gcd2(int a, int b);
+
+int main(void){
+  int a=6, b=21;
+  printf("%d\n", gcd2(a,b));
+  return 0;
+}
